4385fdfc98f50a066b2b2485025d49823ba2d4ba
zm00622 <zarymanning@gmail.com>
2020-02-19 13:17:05 -0500

    Major Changes
    This commit changes content on all of the pages with the exception of the Education page. I have added a theme bar to the bottom of the homepage which will eventually allow the user to change themes. I have also added to my about bio and included links to various projects on the skills page.

---
+++ build/precache-manifest.cac784ad5f324355d8628b65fe6b1b80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b62bf155e1fdd39211346f78ad47a54",
+    "url": "/ink-theme/index.html"
+  },
+  {
+    "revision": "675786fa6363047255d7",
+    "url": "/ink-theme/static/css/main.086862c5.chunk.css"
+  },
+  {
+    "revision": "68baa641bf8dd44182ca",
+    "url": "/ink-theme/static/js/2.46d06792.chunk.js"
+  },
+  {
+    "revision": "2bcf73e3839e96bb488d3fc4da7ab52a",
+    "url": "/ink-theme/static/js/2.46d06792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675786fa6363047255d7",
+    "url": "/ink-theme/static/js/main.ee9858bf.chunk.js"
+  },
+  {
+    "revision": "7ae671a31ccd00bde773",
+    "url": "/ink-theme/static/js/runtime-main.ee089230.js"
+  },
+  {
+    "revision": "d5f16787c9daef6c86ea25edb2f2e025",
+    "url": "/ink-theme/static/media/Ink.d5f16787.png"
+  },
+  {
+    "revision": "188fbd6a6114b14d8aba002cfcc6da74",
+    "url": "/ink-theme/static/media/MyPic.188fbd6a.jpg"
+  }
+]);
